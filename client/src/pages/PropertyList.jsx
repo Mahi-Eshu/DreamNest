9131f856824d9f374c631a8309f376068d45935f
Mahi-Eshu <maheshkumarvenkatesan03@gmail.com>
2024-05-24 23:54:04 +0530

    home rental

---
+++ client/src/pages/PropertyList.jsx
@@ -0,0 +1,85 @@
+import "../styles/List.scss";
+import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../components/Navbar";
+import ListingCard from "../components/ListingCard";
+import { useCallback, useEffect, useState } from "react";
+import { setPropertyList } from "../redux/state";
+import Loader from "../components/Loader";
+import Footer from "../components/Footer"
+
+const PropertyList = () => {
+  const [loading, setLoading] = useState(true)
+  const user = useSelector((state) => state.user)
+  const propertyList = user?.propertyList;
+  const dispatch = useDispatch()
+
+  const getPropertyList = useCallback(async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/users/${user._id}/properties`, {
+        method: "GET"
+      })
+      const data = await response.json()
+      dispatch(setPropertyList(data))
+      setLoading(false)
+    } catch (err) {
+      console.log("Fetch all properties failed", err.message)
+    }
+  }, [dispatch, user._id])
+
+  useEffect(() => {
+    getPropertyList()
+  }, [getPropertyList])
+
+  const handleDelete = async (propertyId) => {
+    try {
+      await fetch(`http://localhost:3001/properties/${propertyId}`, {
+        method: 'DELETE'
+      });
+      // Update the property list in the state
+      getPropertyList(); // Re-fetch the updated list
+    } catch (err) {
+      console.error('Delete property failed', err.message);
+    }
+  };
+
+  return loading ? <Loader /> : (
+    <>
+      <Navbar />
+      <h1 className="title-list">Your Property List</h1>
+      <div className="list">
+        {propertyList?.map(
+          ({
+            _id,
+            creator,
+            listingPhotoPaths,
+            city,
+            province,
+            country,
+            category,
+            type,
+            price,
+            booking = false,
+          }) => (
+            <ListingCard
+              key={_id}
+              listingId={_id}
+              creator={creator}
+              listingPhotoPaths={listingPhotoPaths}
+              city={city}
+              province={province}
+              country={country}
+              category={category}
+              type={type}
+              price={price}
+              booking={booking}
+              onDelete={() => handleDelete(_id)} // Pass delete handler
+            />
+          )
+        )}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default PropertyList;
